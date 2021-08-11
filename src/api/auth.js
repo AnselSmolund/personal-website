0ba0018d79c980d1c581778c7da1e7486cd9a307
anselsmolund <smolu006@umn.edu>
2021-08-11 13:58:17 -0500

    delete auth file

--- src/api/auth.js
+++
@@ -1,6 +0,0 @@
-export const auth = {
-  client_id: "51514",
-  client_secret: "f0c683b56512a211b2d9a6e73e7ec85be147d957",
-  refresh: "9b02d05c12b6aa2271e5f8fa91810a5906268f61",
-  code: "2447b6781d0b6277c67ac1eb94389e9831ea03cb",
-};
